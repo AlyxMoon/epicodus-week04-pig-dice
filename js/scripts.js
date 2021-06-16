43b8a3c1021dee34da8bf24c32df9610d92bcb33
AlyxMoon <allisterkmoon@gmail.com>
2021-06-16 13:48:08 -0700

    begin displaying current score in the UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 
-
+function updatePlayerDisplay (gameManager) {
+  $('#player-1 .current').text(gameManager.players[0].score)
+  $('#player-2 .current').text(gameManager.players[1].score)
+}
 
 const main = () => {
   const gameManager = new GameManager()
 
   $('#player-1 .button-roll').on('click', function (event) {
-    alert('roll for player 1')
+    gameManager.addScoreToPlayer(0, gameManager.rollDice())
+    updatePlayerDisplay(gameManager)
   })
 
   $('#player-1 .button-hold').on('click', function (event) {
-    alert('hold for player 1')
   })
 
   $('#player-2 .button-roll').on('click', function (event) {
-    alert('roll for player 2')
+    gameManager.addScoreToPlayer(1, gameManager.rollDice())
+    updatePlayerDisplay(gameManager)
   })
 
   $('#player-2 .button-hold').on('click', function (event) {
